113918f5f62ef2618ae93cc103c1cad714e2a512
rjn991 <ranjanht991@gmail.com>
2022-05-11 15:08:29 +0530

    updated code

--- heap.py
+++ heap.py
@@ -10,3 +10,18 @@ def heapidy(a,n,i):
 
 	left=2*i+1
 	right=2*i+2
+
+	lar_child=left
+
+	if right<n:
+		if a[right]>a[left]:
+			lar_child=right
+		
+	if a[lar_child]>a[i]:
+		a[i],a[large_child]=a[large_child],a[i]
+	#parent and child
+		heapify(a,n,lar_child)
+	#child mode
+def 
+
+	
