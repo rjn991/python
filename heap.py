1cfdd530a41f62d36cb53d7c415a1a8864ffc689
rjn991 <ranjanht991@gmail.com>
2022-05-11 15:17:00 +0530

    updated

--- heap.py
+++ heap.py
@@ -1,27 +1,33 @@
-# heap sort
-
-def get_data(a,n):
+import random
+def gen_data(a,n):
 	for i in range(n):
-		a.append(random.randint(0,100))
-
-def heapidy(a,n,i):
+		x=random.randint(0,100)
+		a.append(x)
+def heapify(a,n,i):
 	if 2*i+1>=n:
 		return
-
 	left=2*i+1
 	right=2*i+2
-
 	lar_child=left
-
 	if right<n:
 		if a[right]>a[left]:
 			lar_child=right
-		
 	if a[lar_child]>a[i]:
-		a[i],a[large_child]=a[large_child],a[i]
-	#parent and child
+		a[i],a[lar_child]=a[lar_child],a[i]
 		heapify(a,n,lar_child)
-	#child mode
-def 
-
-	
+def heap_sort(a):
+	n=len(a)
+	for i in range(n,-1,-1):
+		heapify(a,n,i)
+	for i in range(n-1,0,-1):
+		a[i],a[0]=a[0],a[i]
+		heapify(a,i,0)
+a=[ ]
+print("enter size: ")
+n=int(input())
+gen_data(a,n)
+print("the numbers are: ")
+print(a)
+heap_sort(a)
+print("the sorted numbers are: ")
+print(a)
